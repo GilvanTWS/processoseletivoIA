b32d7238d5aa7face1b3d9f6ac0032f51bb0f2f7
GilvanTWS <gilvan.junior423@gmail.com>
2026-04-18 01:23:04 +0000

    feat: adicionando carregamento do modelo treinado no optimize_model.py

--- optimize_model.py
+++ optimize_model.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
 import os
 
-#insira seu código aqui
+# 1. Carregando o modelo treinado
+print("Carregando o modelo treinado...")
+model = tf.keras.models.load_model("model.h5")
+print("Modelo carregado com sucesso!")
