7e1aba47daabdb45a2de14ab9fed88d4360498df
GilvanTWS <gilvan.junior423@gmail.com>
2026-04-18 01:25:16 +0000

    feat: aplicando Dynamic Range Quantization para otimização Edge AI

--- optimize_model.py
+++ optimize_model.py
@@ -11,3 +11,9 @@ print("Convertendo modelo para TensorFlow Lite...")
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
 print("Conversão concluída!")
+
+# 3. Aplicando otimização Dynamic Range Quantization
+print("Aplicando otimização Dynamic Range Quantization...")
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+tflite_model_optimized = converter.convert()
+print("Otimização concluída!")
