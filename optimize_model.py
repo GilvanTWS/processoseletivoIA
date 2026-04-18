b91c2dd97277f92448a76a588c2e3f56911ce6e0
GilvanTWS <gilvan.junior423@gmail.com>
2026-04-18 01:24:09 +0000

    feat: adicionando conversão do modelo para formato TFLite

--- optimize_model.py
+++ optimize_model.py
@@ -5,3 +5,9 @@ import os
 print("Carregando o modelo treinado...")
 model = tf.keras.models.load_model("model.h5")
 print("Modelo carregado com sucesso!")
+
+# 2. Convertendo para TensorFlow Lite
+print("Convertendo modelo para TensorFlow Lite...")
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+print("Conversão concluída!")
